821a5ba679bf045feb0dd9f226638e4722c34ef9
Billy Tamplin <billytamplin@gmail.com>
2016-09-13 12:26:28 -0700

    check to see if desktop view only

--- js/thumb-hover-gif.js
+++ js/thumb-hover-gif.js
@@ -1,12 +1,20 @@
-$(document).ready(function()
-{
-    $(".thumbnail").hover(
-        function() {
-          var src = $(this).find(".hover-png").attr("src");
-          $(this).find(".hover-png").attr("src", src.replace(/\.png$/i, ".gif"));
-        },
-        function() {
-          var src = $(this).find(".hover-png").attr("src");
-          $(this).find(".hover-png").attr("src", src.replace(/\.gif$/i, ".png"));
-        });
+$(document).ready(function() {
+ var isDesktop = (function() {
+  return !('ontouchstart' in window) // works on most browsers
+  || !('onmsgesturechange' in window); // works on ie10
+ })();
+ //edit, if you want to use this variable outside of this closure, or later use this:
+ window.isDesktop = isDesktop;
+ if( isDesktop ){
+   $(".thumbnail").hover(
+     function() {
+       var src = $(this).find(".hover-png").attr("src");
+       $(this).find(".hover-png").attr("src", src.replace(/\.png$/i, ".gif"));
+     },
+     function() {
+       var src = $(this).find(".hover-png").attr("src");
+       $(this).find(".hover-png").attr("src", src.replace(/\.gif$/i, ".png"));
+     }
+   );
+ }
 });
