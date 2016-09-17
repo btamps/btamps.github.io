6750b0ced6c47d1adc2f544ec2d1cbfd8560cf20
Billy Tamplin <billytamplin@gmail.com>
2016-09-17 14:10:42 -0700

    added keypress js for next prev links

---
+++ js/keypress.js
@@ -0,0 +1,10 @@
+var next = $('.arrow-nav .next')[0],
+    prev = $('.arrow-nav .prev')[0];
+
+$(window).keydown(function (e) {
+    if ( e.which === 37 ) {
+        window.location.href = next.href;
+    } else if ( e.which === 39 ) {
+        window.location.href = prev.href;
+    }
+});
